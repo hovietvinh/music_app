cb4c28c993043fc924d3bd26d103b427b464a7f3
hovietvinh <hovietvinh29032004@gmail.com>
2024-08-27 13:17:18 +0700

    tao route co ban

--- music_app_be/index.ts
+++ music_app_be/index.ts
@@ -1,1 +1,13 @@
-console.log("ok");
+import express,{Express,Request,Response} from "express"
+
+
+const app: Express = express()
+const port:number = 3000
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
